17c6837b9cef1aefd89c661d508f25518f394afa
Tom Houlé <tom@tomhoule.com>
2020-06-14 14:44:33 +0200

    Finish problem 3

--- src/chap1/problem_3.lean
+++ src/chap1/problem_3.lean
@@ -1,10 +1,12 @@
+import data.rat.basic
 import algebra.ordered_field
+import chap1.problem_1
 
-variables (α : Type) [discrete_linear_ordered_field α] [comm_group α]
+variables {α : Type} [discrete_linear_ordered_field α] [comm_group α]
 variables {a b c d : α}
 
 -- (i)
-def mul_div : b ≠ 0 → c ≠ 0 → a / b = (a*c) / (b*c) :=
+def my_mul_div : b ≠ 0 → c ≠ 0 → a / b = (a*c) / (b*c) :=
 assume hbnotzero cnotzero,
 calc
     a / b   = a * b⁻¹ : rfl
@@ -35,3 +37,74 @@ calc
             ... = (a * b) * (a * b)⁻¹ * a⁻¹ * b⁻¹ : by simp [mul_comm (b⁻¹), mul_assoc]
             ... = 1 * a⁻¹ * b⁻¹ : by rw [mul_inv_cancel abnotzero]
             ... = a⁻¹ * b⁻¹ : by rw [one_mul]
+
+--- (iv)
+def div_div_mul : b ≠ 0 → d ≠ 0 → (a/b) * (c/d) = (a*c)/(b*d) :=
+assume bnotzero dnotzero,
+calc
+    (a/b) * (c/d)   = (a*b⁻¹) * (c*d⁻¹) : rfl
+                ... = (a*c) * (b⁻¹ * d⁻¹) : by simp [mul_comm c, mul_assoc]
+                ... = (a*c) * (b * d)⁻¹ : by rw [mul_inv''' bnotzero dnotzero]
+                ... = (a*c)/(b*d) : rfl
+
+def my_inv_inv : a ≠ 0 → (a⁻¹)⁻¹ = a :=
+assume anzero,
+calc
+    (a⁻¹)⁻¹ = a * a⁻¹ * (a⁻¹)⁻¹ : by simp
+        ... = a * (a⁻¹ * (a⁻¹)⁻¹) : by rw mul_assoc
+        ... = a * (a * (a⁻¹))⁻¹ : by rw mul_inv''
+        ... = a * 1⁻¹ : by rw mul_inv_cancel anzero
+        ... = a : by simp
+
+--- (v)
+def my_div_div : b ≠ 0 → c ≠ 0 → d ≠ 0 → (a/b) / (c/d) = (a*d)/(b*c) :=
+assume bnzero cnzero dnzero,
+calc
+    (a/b) / (c/d)   = (a*b⁻¹) / (c*d⁻¹) : rfl
+                ... = (a*b⁻¹) * (c*d⁻¹)⁻¹ : rfl
+                ... = (a*b⁻¹) * (c⁻¹*(d⁻¹)⁻¹) : by rw [mul_inv'']
+                ... = (a*b⁻¹) * (c⁻¹*d) : by simp [inv_inv]
+                ... = (a*d) * (b⁻¹ * c⁻¹) : by simp [mul_assoc, mul_comm d]
+                ... = (a*d) * (b*c)⁻¹ : by rw mul_inv''
+                ... = (a*d)/(b*c) : rfl
+
+-- (vi)
+theorem my_div_eq : b ≠ 0 → d ≠ 0 → ((a*d) = (b*c) ↔ (a/b) = (c/d)) :=
+assume bnotzero dnotzero,
+iff.intro
+    (   assume (h : a * d = b * c),
+        calc
+            a/b = a * b⁻¹ : rfl
+            ... = a * b⁻¹ * (d * d⁻¹) : by simp [mul_inv_cancel dnotzero]
+            ... = a * d * b⁻¹ * d⁻¹ : by simp [mul_comm d, mul_assoc]
+            ... = b * c * b⁻¹ * d⁻¹ : by rw h
+            ... = b * b⁻¹ * c * d⁻¹ : by simp [mul_comm (b⁻¹), mul_assoc]
+            ... = c * d⁻¹ : by rw [mul_inv_cancel bnotzero, one_mul]
+            ... = c/d : rfl)
+    (   assume (h : a/b = c/d),
+        calc
+            a*d = a * (b⁻¹ * b) * d : by simp [mul_inv_cancel bnotzero, mul_assoc, mul_comm]
+            ... = a * b⁻¹ * b * d : by simp [mul_assoc]
+            ... = a/b * b * d : rfl
+            ... = c/d * b * d : by rw h
+            ... = c * d⁻¹ * b * d : rfl
+            ... = c * b : by simp [mul_assoc, mul_comm (d⁻¹), mul_inv_cancel dnotzero]
+            ... = b * c : by rw mul_comm)
+
+theorem my_div_self : a ≠ 0 → b ≠ 0 → ((a/b) = (b/a) ↔ a = b ∨ a = -b) :=
+assume anotzero bnotzero,
+iff.intro
+    (λ (h : a/b = b/a),
+        have a*a = b*b, from (iff.elim_right (my_div_eq bnotzero anotzero)) h,
+        have a^2 = b^2, by rwa [←pow_two a, ←pow_two b] at this,
+        show a = b ∨ a = -b, from sq_eq_sq this
+    )
+    (λ (h : a = b ∨ a = -b),
+        or.elim h
+            (λ h, by rw h)
+            (λ (h: a = -b),
+                calc
+                a/b = -b / b : by rw h
+                ... = b / -b : by rw [neg_div, div_neg]
+                ... = b / a : by rw h
+            ))
