35119336d4263924a5eb3e7e1a2dd43ad8e322e2
Tom Houlé <tom@tomhoule.com>
2021-02-07 12:37:23 +0100

    Some more struggle with problem 13

--- src/chap2/problem_13.lean
+++ src/chap2/problem_13.lean
@@ -1,13 +1,19 @@
 import data.real.basic
 import data.real.irrational
+import data.int.basic
 
 namespace problem13
 
 open real (sqrt)
+open has_dvd (dvd)
 
-example (a b c : ℝ) : a / 0 = 0 := div_zero a
+lemma div3Sq (a : ℤ) : 3 ∣ a^2 → 3 ∣ a := by {
+  intro h,
+  sorry
+}
 
--- I'm going to skip this for now
+example : 1 ∣ 2 := one_dvd 2
+example (a : ℤ) (p : even a) : 2 ∣ a := even_iff_two_dvd.mp p
 
 theorem sqrt3Irrational : irrational (sqrt 3) := by {
   rintros ⟨⟨num, denom, denomPos, hCoprimes⟩, hEq⟩,
@@ -17,8 +23,10 @@ theorem sqrt3Irrational : irrational (sqrt 3) := by {
   rw [pow_two, rat.num_denom', rat.mk_eq_div, div_mul_div, <-pow_two, <-pow_two] at hEq,
   push_cast at hEq,
   rw div_eq_iff sorry at hEq,
-  have h1 : sqrt ((num:real)^2) = sqrt (3* denom^2) := congr_arg sqrt hEq,
-  rw [real.sqrt_mul sorry, real.sqrt_sqr sorry, real.sqrt_sqr sorry] at h1,
+  have : 3 ∣ (num:real)^2 := dvd.intro (↑denom ^ 2) (eq.symm hEq),
+
+  -- have h1 : sqrt ((num:real)^2) = sqrt (3* denom^2) := congr_arg sqrt hEq,
+  -- rw [real.sqrt_mul sorry, real.sqrt_sqr sorry, real.sqrt_sqr sorry] at h1,
 
   -- rw [pow_two, rat.num_denom', rat.mul_def (ne.symm $ ne_of_lt $ nat.cast_lt.mpr denomPos) (ne.symm $ ne_of_lt $ nat.cast_lt.mpr denomPos)] at hEq,
   sorry
---
+++ src/chap2/problem_14.lean
@@ -0,0 +1,13 @@
+import data.real.irrational
+
+namespace problem14
+
+open real (sqrt)
+
+theorem a : irrational (sqrt 2 + sqrt 6) := by {
+  sorry
+}
+
+theorem b : irrational (sqrt 2 + sqrt 3) := sorry
+
+end problem14
