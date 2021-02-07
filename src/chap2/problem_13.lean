2e1d4d61f0bfac7258127c46e76e3d79a192268f
Tom Houlé <tom@tomhoule.com>
2021-02-07 19:16:29 +0100

    Make progress on problem 13

--- src/chap2/problem_13.lean
+++ src/chap2/problem_13.lean
@@ -7,7 +7,7 @@ namespace problem13
 open real (sqrt)
 open has_dvd (dvd)
 
-lemma div3Sq (a : ℤ) : 3 ∣ a^2 → 3 ∣ a := by {
+lemma div3Sq (a : ℝ) : 3 ∣ a^2 → 3 ∣ a := by {
   intro h,
   sorry
 }
@@ -24,6 +24,19 @@ theorem sqrt3Irrational : irrational (sqrt 3) := by {
   push_cast at hEq,
   rw div_eq_iff sorry at hEq,
   have : 3 ∣ (num:real)^2 := dvd.intro (↑denom ^ 2) (eq.symm hEq),
+  have : 3 ∣ (num:real) := div3Sq (num:real) this,
+  have : 3 ∣ num := sorry,
+  obtain ⟨k, kEq⟩ : ∃ k, k * 3 = (num:real) := ⟨(num:real)/3, div_mul_cancel (num:real) (show (3:real) ≠ 0, by norm_num)⟩,
+
+  rw [<-kEq, pow_two, mul_assoc, mul_comm, mul_assoc] at hEq,
+  simp only [mul_eq_mul_left_iff, (show (3:ℝ) ≠ 0, by norm_num), or_false] at hEq,
+  rw [mul_assoc, mul_comm, mul_assoc, <-pow_two] at hEq,
+
+  have : 3 ∣ (denom:real)^2 := dvd.intro (k ^ 2) hEq,
+  have : 3 ∣ denom := by sorry,
+
+
+  have : int.gcd num denom ≥ 3, by sorry,
 
   -- have h1 : sqrt ((num:real)^2) = sqrt (3* denom^2) := congr_arg sqrt hEq,
   -- rw [real.sqrt_mul sorry, real.sqrt_sqr sorry, real.sqrt_sqr sorry] at h1,
